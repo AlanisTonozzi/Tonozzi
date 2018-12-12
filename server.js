fa809c8cd34af6a8bca2945827dca6729552665e
alanis tonozzi <alan.tonozzi@gmail.com>
2018-12-12 16:01:11 -0200

    Dificuldades preparam pessoas comuns para destinos extraordinários

---
+++ server.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const express = require('express')
+
+const server = express()
+
+server.use(express.static(__dirname + '/src'))
+
+server.listen(3000, () =>{
+    console.log("Servidor rodando na porta 3000")
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const server = http.createServer((req,res)=> {
+    if(req.url == "/") { 
+        res.end("<h1> foi1 </h1>")
+
+    }
+    if(req.url =="/contato"){
+       
+    }
+    res.end("<h1> Sem respota para a URL</h1>")
+})
+
+server.listen(3000, "localhost", () => {
+    console.log("funcionou")
+
+})*/
