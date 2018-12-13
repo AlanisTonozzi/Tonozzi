7c08d86b8eb20a047007f153989ed0486517fcfc
alanis tonozzi <alan.tonozzi@gmail.com>
2018-12-13 16:25:17 -0200

    Aprendendo com os erros e me superando com as lições

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,17 @@
- const gulp = require("gulp")
- const htmlmin = require("gulp-htmlmin")
- const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp")
+const htmlmin = require("gulp-htmlmin")
+const cleanCSS = require('gulp-clean-css');
 
- const files = {
-	htmlmin: ['./src/*.html'],
- };
+gulp.task('minify', () => {
+return gulp.src('src/*.html')
+	.pipe(htmlmin({ collapseWhitespace: true }))
+	.pipe(gulp.dest('dist'));
+});
 
- gulp.task('minify', () => {
-	return gulp.src('src/*.html')
-	  .pipe(htmlmin({ collapseWhitespace: true }))
-	  .pipe(gulp.dest('dist'));
-  });
+gulp.task('minify-css', () => {
+return gulp.src('./src/**/*.css')
+	.pipe(cleanCSS({compatibility: 'ie8'}))
+	.pipe(gulp.dest('dist'));
+});
 
-  gulp.task('minify-css', () => {
-	return gulp.src('styles/*.css')
-	  .pipe(cleanCSS({compatibility: 'ie8'}))
-	  .pipe(gulp.dest('dist'));
-  });
-  
-
+gulp.task("build", gulp.parallel(["minify", "minify-css"]))
