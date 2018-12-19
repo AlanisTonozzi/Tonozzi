409194575dd84e1c1eac3fdeeca7cfa826f5f39b
alanis tonozzi <alan.tonozzi@gmail.com>
2018-12-19 18:02:17 -0200

    Falta de sono = excesso de pensamentos

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp")
 const htmlmin = require("gulp-htmlmin")
 const cleanCSS = require('gulp-clean-css');
 
+
 gulp.task('minify', () => {
 return gulp.src('src/*.html')
 	.pipe(htmlmin({ collapseWhitespace: true }))
@@ -15,3 +16,9 @@ return gulp.src('./src/**/*.css')
 });
 
 gulp.task("build", gulp.parallel(["minify", "minify-css"]))
+
+
+
+gulp.task("build:watch", () => {
+	gulp.watch('./src/**/*.*', gulp.series(['build']))
+})
